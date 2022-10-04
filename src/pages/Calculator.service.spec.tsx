3721eeba7139e711c3b1f864ef0edbd879b149d8
Robson Arruda <robsonshockwavegithub@gmail.com>
2022-10-03 23:32:28 -0300

    function concatenate number

--- src/pages/Calculator.service.spec.tsx
+++ src/pages/Calculator.service.spec.tsx
@@ -1,7 +1,7 @@
 import { CalculatorService } from './Calculator.service';
 
 describe('Teste do CalculadoraService', () => {
-  const [calculate] = CalculatorService();
+  const { calculate, concatenateNumber } = CalculatorService();
 
   it('deve garantir que 1 + 4 = 5', () => {
     let sum = calculate(1, 4, '+');
--- src/pages/Calculator.service.ts
+++ src/pages/Calculator.service.ts
@@ -16,5 +16,24 @@ export function CalculatorService() {
       : result[operator as keyof typeof result];
   }
 
-  return [calculate];
+  function concatenateNumber(currentNumber: string, concatNumber: string) {
+    // caso contenha apenas '0' ou null, reinicie o valor
+    if (currentNumber === '0' || currentNumber === null) {
+      currentNumber = '';
+    }
+
+    // primeiro digito for '.', concatena '0' antes do ponto
+    if (concatNumber === '.' && currentNumber === '') {
+      return '0.';
+    }
+
+    // caso '.' digitado e já contenha um '.', apenas retorna
+    if (concatNumber === '.' && currentNumber.indexOf('.') > -1) {
+      return currentNumber;
+    }
+
+    return currentNumber + concatNumber;
+  }
+
+  return { calculate, concatenateNumber };
 }
