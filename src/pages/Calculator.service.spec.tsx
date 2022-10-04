954ffc4a851eef61ce0331bb7fb089ad328d6ea5
Robson Arruda <robsonshockwavegithub@gmail.com>
2022-10-03 23:18:23 -0300

    tests

--- src/pages/Calculator.service.spec.tsx
+++ src/pages/Calculator.service.spec.tsx
@@ -7,4 +7,24 @@ describe('Teste do CalculadoraService', () => {
     let sum = calculate(1, 4, '+');
     expect(sum).toEqual(5);
   });
+
+  it('deve garantir que 1 - 4 = -3', () => {
+    let sum = calculate(1, 4, '-');
+    expect(sum).toEqual(-3);
+  });
+
+  it('deve garantir que 1 / 4 = 0.25', () => {
+    let sum = calculate(1, 4, '/');
+    expect(sum).toEqual(0.25);
+  });
+
+  it('deve garantir que 1 * 4 = 4', () => {
+    let sum = calculate(1, 4, '*');
+    expect(sum).toEqual(4);
+  });
+
+  it('deve retornar 0 para operação inválida', () => {
+    let sum = calculate(1, 4, '%');
+    expect(sum).toEqual(0);
+  });
 });
--- src/pages/Calculator.service.ts
+++ src/pages/Calculator.service.ts
@@ -1,13 +1,19 @@
 export function CalculatorService() {
   function calculate(number1: number, number2: number, operator: string) {
+    const operatorInvalid =
+      !operator || !['+', '-', '*', '/'].includes(operator);
+
     const result = {
       '+': number1 + number2,
       '-': number1 - number2,
       '*': number1 * number2,
       '/': number1 / number2,
+      operatorInvalid: 0,
     };
 
-    return result[operator as keyof typeof result];
+    return operatorInvalid
+      ? result['operatorInvalid']
+      : result[operator as keyof typeof result];
   }
 
   return [calculate];
